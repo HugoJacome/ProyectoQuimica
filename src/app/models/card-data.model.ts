078cb333f83367824a906b30d36c1fab5e38fe60
Hugo Jacome <hjacome@prosupply.ec>
2021-08-27 17:20:50 -0500

    Auto-generated commit [ci skip]

--- src/app/models/card-data.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ElementData {
-    element: string;
-    symbol: string;
-    oxidationNumber: string;
-}
-export interface CardData {
-    element: string;
-    state: 'default' | 'flipped' | 'matched';
-}
