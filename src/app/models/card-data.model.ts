757478d3c7eadcc2962965b7521a8e0ae8c12ab7
Hugo Jacome <hjacome@prosupply.ec>
2021-08-27 22:06:49 -0500

    deploy prod

--- src/app/models/card-data.model.ts
+++ src/app/models/card-data.model.ts
@@ -2,6 +2,7 @@ export interface ElementData {
     element: string;
     symbol: string;
     oxidationNumber: string;
+    state?: 'default' | 'flipped' | 'matched';
 }
 export interface CardData {
     element: string;
